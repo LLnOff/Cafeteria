48f55f88da20669b3e64f22e7855749a92d213b7
Lucas <lucks9616@gmail.com>
2022-09-13 22:22:27 -0300

    tem que por as dependencias compile 'com.android.support:cardview-v7:21.0.+' compile 'com.android.support:recyclerview-v7:21.0.+'

--- app/src/main/java/br/etecia/mycafeteria/MainActivity.java
+++ app/src/main/java/br/etecia/mycafeteria/MainActivity.java
@@ -1,11 +1,12 @@
 package br.etecia.mycafeteria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView rv = findViewById(R.id.rv);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
